486950b2348b458c8dff7d8f87ee75b821924a92
RabiaLaduniMukti <rabialadunimukti10@gmail.com>
2023-06-13 23:00:51 +0800

    Membuat Class Mahasiswa dan MahasiswaBeraksi

---
+++ src/universitas/mahasiswa.java
@@ -0,0 +1,20 @@
+package universitas;
+
+public class mahasiswa {
+    //Membuat atribut
+    String nama, npm,prodi ;
+    
+    public String getDetail(){
+        return nama+npm+prodi;
+    }
+        public mahasiswa (String nama, String npm, String prodi){
+            this.nama = nama;
+            this.npm = npm;
+            this.prodi = prodi;
+        }
+    
+   
+    }
+
+            
+
